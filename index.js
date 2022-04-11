a6d1c9c78168be88291e8cf8ad86b9e9be4270c3
YefersonR <Yefersonrubio27@gmail.com>
2022-04-11 11:11:01 -0400

    Second Commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express")
-
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.json())
+
 app.get('/',(req,res)=>{
-        res.send("hola mundo")
+    const usuario = {nombre: "Yeferson", apellido:"Rubio"}
+    res.json(usuario)
 })
-app.post('/')
+app.post('/adduser',(req,res)=>{
+    const usuario = req.body
+    usuario.nombre = usuario.nombre
+    usuario.telefono = "809-223-5612"
+    res.json(usuario)
+})
 app.listen(3000,()=>{
     console.log("Ya se creo el server")
 })
